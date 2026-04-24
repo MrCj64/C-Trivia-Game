1a5d4cf39d75c2dfbd5c8016c1b60f235c48ce2a
Rafael-Ale <alumnoesca41@gmail.com>
2026-04-24 12:48:47 -0600

    First file of API/Server handler, not finished

---
+++ triviaAPI/Server.py
@@ -0,0 +1,32 @@
+
+import sqlalchemy as db
+
+user = "root"
+password = "root"
+host = "127.0.0.1"
+port  = 3306
+database = "triviagamebd"
+metadata_obj = db.MetaData()
+
+def get_connection():
+    engine = db.create_engine(
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}", echo =True
+    )
+    return engine
+
+# def get_table():
+
+#     #Definir un nombre de tabla:
+
+try:
+    engine = get_connection()
+    print(f"Connection to the {host} for user {user} created succesfully.")
+except Exception as ex:
+    print("Connection could not be made due to the following error:\n",ex)
+
+# Instalar la libreria de sqlalchemy: pip install sqlalchemy ✓ 
+# Instalar la liberia de pymysql: pip install mysql ✓
+# Crear un motor para conectarse ✓
+# Crear las tablas
+# Crear las queries
+# Crear la API
